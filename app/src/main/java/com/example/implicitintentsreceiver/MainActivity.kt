139a9befd14fb208c5ce3cf9e2404a18f0a63da3
Aleksandar Ignatov <ani1802@abv.bg>
2019-12-06 22:01:23 +0200

    Add action.VIEW intent receiving functionality

--- app/src/main/java/com/example/implicitintentsreceiver/MainActivity.kt
+++ app/src/main/java/com/example/implicitintentsreceiver/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.implicitintentsreceiver
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        intent.data?.let {uri ->
+            val uriString = getString(R.string.uri_label) + uri.toString()
+            val textView = findViewById<TextView>(R.id.text_uri_message)
+            textView.text = uriString
+        }
     }
 }
